56b27fac19d032cd7fd9891df30b8d85c91644ac
Andriy Mulyar <andriy.mulyar@gmail.com>
2018-07-24 15:36:57 -0400

    Implemented depth first aggregation

--- sklearn/ensemble/branching_tree_dev.py
+++ sklearn/ensemble/branching_tree_dev.py
@@ -11,7 +11,7 @@ import numpy as np
 
 class BranchingTreeEnsemble(BaseEnsemble):
 
-    def __init__(self, architect_tree, base_estimator,  combination_method="majority_vote", depth_first=False, random_state=None):
+    def __init__(self, architect_tree, base_estimator,  combination_method="majority_vote", depth_first=False, use_architect=False, random_state=None):
         """
 
         :param architect_tree: The DecisionTree that will architect the ensemble structure
@@ -29,6 +29,7 @@ class BranchingTreeEnsemble(BaseEnsemble):
         self.random_state = random_state
         self.method = combination_method
         self.depth_first = depth_first
+        self.use_architect = use_architect
         self.architect_tree.random_state = random_state
 
 
@@ -93,29 +94,49 @@ class BranchingTreeEnsemble(BaseEnsemble):
         # print(number_non_leaf_nodes)
 
         self.estimators_ = [] #holds all base learners
-        self.estimator_map = {} #maps indices in self.estimators to tree node indices
+        self.estimator_to_node_map = {} #maps indices in self.estimators to tree node indices
+        self.node_to_estimator_map = {}
 
         #we begin by fitting estimators on all decision nodes for testing
         for i in range(n_nodes):
             if not is_leaf[i]:
                 #print("Creating %i" % i)
-                self.estimator_map[len(self.estimators_)] = i
+                self.estimator_to_node_map[len(self.estimators_)] = i
+                self.node_to_estimator_map[i] = len(self.estimators_)
                 estimator = self._make_estimator(append = True, random_state=self.random_state)
                 estimator.fit(X[samples[i]], y[samples[i]])
                 #print("Fit %i" % i)
+        #print(self.node_to_estimator_map)
 
     def predict_architect_(self, X):
         return self.architect_tree.predict(X)
 
     def _predict(self, X):
         """Collect results from clf.predict calls. """
-
+        all_predictions = [clf.predict(X) for clf in self.estimators_]
         if self.depth_first:
-            dec_paths = self.architect.decision_path(X)
-            pass
+            all_predictions = []
+            dec_paths = self.architect_tree.decision_path(X)
+            for sample_index in range(len(X)):
+                node_indices = dec_paths.indices[dec_paths.indptr[sample_index]:dec_paths.indptr[sample_index + 1]]
+                estimator_indices = []
+                for node_index in node_indices:
+                    if node_index in self.node_to_estimator_map: #if key does not exist, node is a leaf node
+                        estimator_indices.append(self.node_to_estimator_map[node_index]) #add estimator indicies for sample
+
+                # print(sample_index)
+                # print(node_indices)
+                # print(estimator_indices)
+                estimators_on_path = [self.estimators_[i] for i in estimator_indices]
+                sample_predications = [clf.predict([X[sample_index]])[0] for clf in estimators_on_path]
+                if self.use_architect:
+                    sample_predications.append(self.architect_tree.predict([X[sample_index]])[0])
+                all_predictions.append(np.asarray(sample_predications,dtype='int64'))
+            return np.asarray(all_predictions).T
         else:
             all_predictions = [clf.predict(X) for clf in self.estimators_]
-            #all_predictions.append(self.predict_architect_(X))
+            if self.use_architect:
+                all_predictions.append(self.predict_architect_(X))
             return np.asarray(all_predictions).T
 
 
@@ -133,14 +154,15 @@ class BranchingTreeEnsemble(BaseEnsemble):
             return self.weighted_majority_vote_prediction_(X)
 
     def majority_vote_prediction_(self, X):
-        print(X)
         predictions = self._predict(X)
-        for row in predictions:
-            print(row)
-        maj = np.apply_along_axis(
-            lambda x: np.argmax(
-                np.bincount(x, weights=None)),
-            axis=1, arr=predictions)
+        #print(predictions)
+        if self.depth_first:
+            maj = [np.argmax(np.bincount(x)) for x in predictions]
+        else:
+            maj = np.apply_along_axis(
+                lambda x: np.argmax(
+                    np.bincount(x, weights=None)),
+                axis=1, arr=predictions)
 
         return maj
 
